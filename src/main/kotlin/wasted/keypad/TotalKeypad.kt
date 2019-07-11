a874b6699754ef2799b6f217a925a66e4c0f0b66
matvey <msmych@yandex.com>
2019-07-11 22:19:27 +0300

    Adjust visualisation of total with little category amount

--- src/main/kotlin/wasted/keypad/TotalKeypad.kt
+++ src/main/kotlin/wasted/keypad/TotalKeypad.kt
@@ -27,32 +27,36 @@ class TotalKeypad {
     fun send(chatId: Long, total: List<Total>, type: Type = MONTH) {
         bot.execute(SendMessage(chatId, getText(total, type))
             .setParseMode(MARKDOWN)
-            .setReplyMarkup(getInlineKeyboardMarkup(type)))
+            .setReplyMarkup(inlineKeyboardMarkup(type)))
     }
 
     private fun getText(total: List<Total>, type: Type): String {
-        return "${getTitle(type)}\n\n" +
+        return "${title(type)}\n\n" +
                 total.groupBy { it.currency }
                     .map { cur ->
                         val curSum = cur.value.map { it.amount }.sum()
                         "${formatAmount(curSum, Currency.getInstance(cur.key))}\n" +
                                 cur.value.sortedByDescending { it.amount }
-                                    .joinToString("\n") {
-                                    "${formatAmount(it.amount, Currency.getInstance(it.currency))} ${it.category.emoji.code}\n" +
-                                            it.category.emoji.code
-                                                .repeat((10 * it.amount.toDouble() / curSum.toDouble()).toInt())
-                                }
+                                    .joinToString("\n") { totalItem(it, curSum) }
                     }.joinToString("\n\n")
     }
 
-    private fun getTitle(type: Type): String {
+    private fun title(type: Type): String {
         return "#total " + when (type) {
             MONTH -> "${now().month.getDisplayName(TextStyle.FULL, Locale.US)} ${now().year}"
             ALL -> ""
         }
     }
 
-    private fun getInlineKeyboardMarkup(type: Type): InlineKeyboardMarkup {
+    private fun totalItem(cat: Total, curSum: Long): String {
+        var s = "${formatAmount(cat.amount, Currency.getInstance(cat.currency))} ${cat.category.emoji.code}"
+        if (10 * cat.amount / curSum > 0)
+            s += "\n" + cat.category.emoji.code
+                .repeat((10 * cat.amount.toDouble() / curSum.toDouble()).toInt())
+        return s
+    }
+
+    private fun inlineKeyboardMarkup(type: Type): InlineKeyboardMarkup {
         return InlineKeyboardMarkup()
             .setKeyboard(listOf(listOf(
                 ikb(if (type == MONTH) "${RADIO_BUTTON.code} Month" else "Month", "totalMONTH"),
@@ -65,6 +69,6 @@ class TotalKeypad {
             .setMessageId(messageId)
             .setText(getText(total, type))
             .setParseMode(MARKDOWN)
-            .setReplyMarkup(getInlineKeyboardMarkup(type)))
+            .setReplyMarkup(inlineKeyboardMarkup(type)))
     }
 }
