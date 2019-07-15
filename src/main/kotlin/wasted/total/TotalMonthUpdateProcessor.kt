09ac1e1dace4916f15bcaa70b6c0c414c088bd4f
matvey <msmych@yandex.com>
2019-07-15 21:29:00 +0300

    Logging message text

--- src/main/kotlin/wasted/total/TotalMonthUpdateProcessor.kt
+++ src/main/kotlin/wasted/total/TotalMonthUpdateProcessor.kt
@@ -2,6 +2,7 @@ package wasted.total
 
 import com.google.inject.Inject
 import com.google.inject.Singleton
+import org.slf4j.LoggerFactory
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
 import wasted.bot.update.processor.UpdateProcessor
@@ -10,6 +11,8 @@ import wasted.total.Total.Type.MONTH
 
 @Singleton
 class TotalMonthUpdateProcessor : UpdateProcessor {
+
+    private val logger = LoggerFactory.getLogger(TotalMonthUpdateProcessor::class.java)
 
     @Inject
     lateinit var bot: TelegramLongPollingBot
@@ -21,6 +24,7 @@ class TotalMonthUpdateProcessor : UpdateProcessor {
     override fun appliesTo(update: Update): Boolean {
         val message = update.message ?: return false
         val text = message.text ?: return false
+        logger.info("Received message $text")
         return text == "/total" || text == "/total@${bot.botUsername}"
     }
 
