ae301b6b5dd07bfd8fec3758a9b3a972e0b09c3c
matvey <msmych@yandex.com>
2019-08-16 21:33:19 +0300

    Changed ENTERTAINMENT emoji to beer

--- src/main/kotlin/wasted/bot/Emoji.kt
+++ src/main/kotlin/wasted/bot/Emoji.kt
@@ -2,44 +2,45 @@ package wasted.bot
 
 enum class Emoji(val code: String) {
 
-    HOUSE("\uD83C\uDFE0"),
-    PIZZA("\uD83C\uDF55"),
-    BOOKS("\uD83D\uDCDA"),
-    GIFT("\uD83C\uDF81"),
-    SHOPPING_CART("\uD83D\uDED2"),
-    AIRPLANE("✈️"),
-    PILL("\uD83D\uDC8A"),
-    STEAM_LOCOMOTIVE("\uD83D\uDE82"),
-    GUITAR("\uD83C\uDFB8"),
-    TELESCOPE("\uD83D\uDD2D"),
-    TABLE_TENNIS("\uD83C\uDFD3"),
-    LIPSTICK("\uD83D\uDC84"),
-    BAR_CHART("📊"),
+  HOUSE("\uD83C\uDFE0"),
+  PIZZA("\uD83C\uDF55"),
+  BOOKS("\uD83D\uDCDA"),
+  GIFT("\uD83C\uDF81"),
+  SHOPPING_CART("\uD83D\uDED2"),
+  AIRPLANE("✈️"),
+  PILL("\uD83D\uDC8A"),
+  STEAM_LOCOMOTIVE("\uD83D\uDE82"),
+  GUITAR("\uD83C\uDFB8"),
+  BEER("\uD83C\uDF7A"),
+  TELESCOPE("\uD83D\uDD2D"),
+  TABLE_TENNIS("\uD83C\uDFD3"),
+  LIPSTICK("\uD83D\uDC84"),
+  BAR_CHART("📊"),
 
-    WHITE_CHECK_MARK("✅"),
-    BALLOT_BOX_WITH_CHECK("☑️"),
-    RADIO_BUTTON("\uD83D\uDD18"),
-    X("❌"),
-    GREY_QUESTION("❔"),
-    HEAVY_MULTIPLICATION_X("✖️"),
-    ARROW_LEFT("⬅️"),
-    ARROW_BACKWARD("◀️"),
-    E1234("\uD83D\uDD22"),
+  WHITE_CHECK_MARK("✅"),
+  BALLOT_BOX_WITH_CHECK("☑️"),
+  RADIO_BUTTON("\uD83D\uDD18"),
+  X("❌"),
+  GREY_QUESTION("❔"),
+  HEAVY_MULTIPLICATION_X("✖️"),
+  ARROW_LEFT("⬅️"),
+  ARROW_BACKWARD("◀️"),
+  E1234("\uD83D\uDD22"),
 
-    ONE("1️⃣"),
-    TWO("2️⃣"),
-    THREE("3️⃣"),
-    FOUR("4️⃣"),
-    FIVE("5️⃣"),
-    SIX("6️⃣"),
-    SEVEN("7️⃣"),
-    EIGHT("8️⃣"),
-    NINE("9️⃣"),
-    ZERO("0️⃣");
+  ONE("1️⃣"),
+  TWO("2️⃣"),
+  THREE("3️⃣"),
+  FOUR("4️⃣"),
+  FIVE("5️⃣"),
+  SIX("6️⃣"),
+  SEVEN("7️⃣"),
+  EIGHT("8️⃣"),
+  NINE("9️⃣"),
+  ZERO("0️⃣");
 
-    companion object {
-        fun fromCode(code: String): Emoji? {
-            return values().firstOrNull{ it.code == code }
-        }
+  companion object {
+    fun fromCode(code: String): Emoji? {
+      return values().firstOrNull { it.code == code }
     }
+  }
 }
--- src/main/kotlin/wasted/expense/Expense.kt
+++ src/main/kotlin/wasted/expense/Expense.kt
@@ -3,36 +3,38 @@ package wasted.expense
 import wasted.bot.Emoji
 import java.util.*
 
-data class Expense(val id: Long,
-                   val userId: Int,
-                   val groupId: Long,
-                   val telegramMessageId: Int?,
-                   val amount: Long,
-                   val currency: String,
-                   val category: Category,
-                   val date: Date) {
+data class Expense(
+  val id: Long,
+  val userId: Int,
+  val groupId: Long,
+  val telegramMessageId: Int?,
+  val amount: Long,
+  val currency: String,
+  val category: Category,
+  val date: Date
+) {
 
-    enum class Category(val emoji: Emoji) {
+  enum class Category(val emoji: Emoji) {
 
-        GROCERIES(Emoji.PIZZA),
-        SHOPPING(Emoji.SHOPPING_CART),
-        TRANSPORT(Emoji.STEAM_LOCOMOTIVE),
-        HOME(Emoji.HOUSE),
-        FEES(Emoji.BAR_CHART),
-        ENTERTAINMENT(Emoji.GUITAR),
-        TRAVEL(Emoji.AIRPLANE),
-        HEALTH(Emoji.PILL),
-        CAREER(Emoji.BOOKS),
-        GIFTS(Emoji.GIFT),
-        SPORT(Emoji.TABLE_TENNIS),
-        HOBBIES(Emoji.TELESCOPE),
-        BEAUTY(Emoji.LIPSTICK),
-        OTHER(Emoji.GREY_QUESTION);
+    GROCERIES(Emoji.PIZZA),
+    SHOPPING(Emoji.SHOPPING_CART),
+    TRANSPORT(Emoji.STEAM_LOCOMOTIVE),
+    HOME(Emoji.HOUSE),
+    FEES(Emoji.BAR_CHART),
+    ENTERTAINMENT(Emoji.BEER),
+    TRAVEL(Emoji.AIRPLANE),
+    HEALTH(Emoji.PILL),
+    CAREER(Emoji.BOOKS),
+    GIFTS(Emoji.GIFT),
+    SPORT(Emoji.TABLE_TENNIS),
+    HOBBIES(Emoji.TELESCOPE),
+    BEAUTY(Emoji.LIPSTICK),
+    OTHER(Emoji.GREY_QUESTION);
 
-        companion object {
-            fun fromName(name: String): Category? {
-                return values().firstOrNull { it.name == name }
-            }
-        }
+    companion object {
+      fun fromName(name: String): Category? {
+        return values().firstOrNull { it.name == name }
+      }
     }
+  }
 }
